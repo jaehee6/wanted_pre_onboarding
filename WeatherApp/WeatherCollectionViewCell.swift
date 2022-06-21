530830615a8a684a2dc55ba0b95ec6a8b31a88f4
jaehee6 <0jujh8@gmail.com>
2022-06-21 17:52:56 +0900

    첫화면collectionview 생성 및 IBOutlet 연결

---
+++ WeatherApp/WeatherCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by 이재희 on 2022/06/21.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var weatherIconImage: UIImageView!
+    @IBOutlet var humidityLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+}
